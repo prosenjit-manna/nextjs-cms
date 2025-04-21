a6f8e8b98126c1a0e8a36b5292aba7afd5909fe8
Prosenjit Manna <prosenjit@itobuz.com>
2025-04-21 19:22:23 +0530

    setup done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // custom rules 
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn", // Warns when 'any' is used
+      "@typescript-eslint/explicit-module-boundary-types": "warn", // Warns when functions lack explicit return types
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }], // Warns for unused variables, ignoring those prefixed with '_'
+      "@typescript-eslint/strict-boolean-expressions": "warn", // Encourages stricter boolean expressions
+      "@typescript-eslint/no-inferrable-types": "warn", // Warns when types are unnecessarily specified
+    },
+  }
 ];
 
 export default eslintConfig;
